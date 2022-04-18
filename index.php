9d56a270fa4dcd4e98f53c2e9150db321e489f10
hailm <hai.lemanh.1310@gmail.com>
2022-04-18 10:48:45 +0700

    feat: block SINH VIÊN, PHỤ HUYNH, DOANH NGHIỆP NÓI GÌ?

--- index.php
+++ index.php
@@ -2,6 +2,106 @@
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
+<!--SINH VIÊN, PHỤ HUYNH, DOANH NGHIỆP NÓI GÌ?-->
+<div class="uk-section home__sinhvien__section uk-light">
+    <div class="uk-container">
+        <div class="item__30">
+            <h3 class="uk-text-center home__trainghiem__title">SINH VIÊN, PHỤ HUYNH, DOANH NGHIỆP NÓI GÌ?</h3>
+        </div>
+        <div class="item__30">
+            <div uk-slider>
+
+                <div class="uk-position-relative">
+
+                    <div class="uk-slider-container uk-light">
+                        <ul class="uk-slider-items uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-small uk-grid-30-m uk-grid-match" uk-grid>
+                            <?php
+                            $data = array(
+                                array(
+                                    'src' => 'images/doingu/1x/img1.png',
+                                    'txt1' => 'Hồ Nguyên Lý',
+                                    'txt2' => 'Sinh viên ngành Ngôn ngữ Hàn',
+                                    'desc' => '“Chương trình đào tạo chất lượng sát với thực tế, giáo trình chuẩn Seoul, mình nghĩ là vừa sức với nhịp độ học tập của mình. Giảng viên chủ yếu dạy thực tiễn, truyền thụ cho tụi mình rất nhiều kinh nghiệm. Nếu được chọn thêm lần nữa mình vẫn chọn chuyên ngành này vì mình rất yêu tiếng Hàn và đất nước Hàn Quốc”.',
+                                ),
+                                array(
+                                    'src' => 'images/doingu/1x/img2.png',
+                                    'txt1' => 'Nguyễn Thị Quỳnh Hoa',
+                                    'txt2' => 'Sinh viên ngành Ngôn ngữ Nhật',
+                                    'desc' => '“Giáo trình của trường FPT phải nói là rất xịn Xò, vừa đầy đủ lại vừa hay, nếu học tập chăm chỉ ba quyển chính thì có thể thi đỗ N2. Về giảng viên thì được học cả người Nhật lẫn người Việt.  Các thầy cô ngành ngôn ngữ cực thân thiện và tâm lý, chia sẻ không chỉ kiến thức học mà còn cả cuộc sống bên Nhật. Nói chung để đánh giá về giảng viên ngành ngôn ngữ Nhật thì 10/10 luôn, vừa thân thiện vừa gần gũi và siêu đáng yêu”.',
+                                ),
+                                array(
+                                    'src' => 'images/doingu/1x/img3.png',
+                                    'txt1' => 'Lê Phạm Thế Vũ',
+                                    'txt2' => 'Sinh viên ngành Ngôn ngữ Anh',
+                                    'desc' => '“Trường Đại học FPT chính là chiếc nôi, giúp bạn phát triển những thế mạnh của bản thân, có thể tự tin nói trước công chúng, khả năng giao tiếp tiếng anh, nuôi dưỡng tài năng Thể thao và Nghệ thuật. Những thầy cô đều là cha mẹ của mình, những người bạn đều xem mình như người anh em”.',
+                                ),
+                                array(
+                                    'src' => 'images/doingu/1x/img1.png',
+                                    'txt1' => 'Hồ Nguyên Lý',
+                                    'txt2' => 'Sinh viên ngành Ngôn ngữ Hàn',
+                                    'desc' => '“Chương trình đào tạo chất lượng sát với thực tế, giáo trình chuẩn Seoul, mình nghĩ là vừa sức với nhịp độ học tập của mình. Giảng viên chủ yếu dạy thực tiễn, truyền thụ cho tụi mình rất nhiều kinh nghiệm. Nếu được chọn thêm lần nữa mình vẫn chọn chuyên ngành này vì mình rất yêu tiếng Hàn và đất nước Hàn Quốc”.',
+                                ),
+                                array(
+                                    'src' => 'images/doingu/1x/img2.png',
+                                    'txt1' => 'Nguyễn Thị Quỳnh Hoa',
+                                    'txt2' => 'Sinh viên ngành Ngôn ngữ Nhật',
+                                    'desc' => '“Giáo trình của trường FPT phải nói là rất xịn Xò, vừa đầy đủ lại vừa hay, nếu học tập chăm chỉ ba quyển chính thì có thể thi đỗ N2. Về giảng viên thì được học cả người Nhật lẫn người Việt.  Các thầy cô ngành ngôn ngữ cực thân thiện và tâm lý, chia sẻ không chỉ kiến thức học mà còn cả cuộc sống bên Nhật. Nói chung để đánh giá về giảng viên ngành ngôn ngữ Nhật thì 10/10 luôn, vừa thân thiện vừa gần gũi và siêu đáng yêu”.',
+                                ),
+                                array(
+                                    'src' => 'images/doingu/1x/img3.png',
+                                    'txt1' => 'Lê Phạm Thế Vũ',
+                                    'txt2' => 'Sinh viên ngành Ngôn ngữ Anh',
+                                    'desc' => '“Trường Đại học FPT chính là chiếc nôi, giúp bạn phát triển những thế mạnh của bản thân, có thể tự tin nói trước công chúng, khả năng giao tiếp tiếng anh, nuôi dưỡng tài năng Thể thao và Nghệ thuật. Những thầy cô đều là cha mẹ của mình, những người bạn đều xem mình như người anh em”.',
+                                ),
+                            );
+                            foreach ($data as $k=>$v): ?>
+                                <li>
+                                    <div class="uk-card uk-card-default home__sinhvien__card uk-flex uk-flex-column">
+                                        <div class="uk-card-body home__sinhvien__card__body uk-flex-auto">
+                                            <div class="home__sinhvien__card__desc"><?= $v['desc'] ?></div>
+                                        </div>
+                                        <div class="uk-card-footer home__sinhvien__card__footer">
+                                            <div class="home__sinhvien__card__box">
+                                                <div class="uk-grid-10 uk-flex-middle" uk-grid>
+                                                    <div class="uk-width-auto">
+                                                        <div class="uk-cover-container uk-border-circle">
+                                                            <img src="<?= $v['src'] ?>" alt="" uk-cover="">
+                                                            <canvas width="76" height="76"></canvas>
+                                                        </div>
+                                                    </div>
+                                                    <div class="uk-width-expand">
+                                                        <div class="home__doingu__card__txt uk-text-uppercase"><?= $v['txt1'] ?></div>
+                                                        <div class="home__doingu__card__txt"><?= $v['txt2'] ?></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                    <div class="uk-hidden@s uk-light">
+                        <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                        <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                    </div>
+
+                    <div class="uk-visible@s">
+                        <a class="uk-position-center-left-out uk-position-small home__doingu__slidenav home__doingu__slidenav--prev" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                        <a class="uk-position-center-right-out uk-position-small home__doingu__slidenav home__doingu__slidenav--next" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                    </div>
+
+                </div>
+
+                <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!--/SINH VIÊN, PHỤ HUYNH, DOANH NGHIỆP NÓI GÌ?-->
+
 <!--ĐỘI NGŨ GIẢNG VIÊN-->
 <div class="uk-section home__doingu__section">
     <div class="uk-container">
